b9a1fcd19318f0666d14cfc3d5ac4266f0da064b
Akobir Abduganiev <akobir.abduganiev@ya.ru>
2022-10-16 17:47:29 +0500

    success

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+        int divider = 5;
+        double result = (double) toBeDivided / divider;
+        System.out.println(result);
     }
 }
